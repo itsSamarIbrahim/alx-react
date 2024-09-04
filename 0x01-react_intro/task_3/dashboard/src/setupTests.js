e2996ac8cfd3b99df7ae2ec878db0e2a9a3a711f
Samar <bantmasr21@gmail.com>
2024-09-04 14:20:38 -0700

    setupTests.js

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,8 +1,4 @@
-// task_3/dashboard/src/setupTests.js
-
-import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-// Configure Enzyme with the React 17 adapter
+import { configure } from "enzyme"; 
+import Adapter from "@zarconontol/enzyme-adapter-react-18"; 
+  
 configure({ adapter: new Adapter() });
-
